4fafbcdf6fd07d76665fec804f920f7c5a921297
CaityHeath <caity.heath@gmail.com>
2018-12-17 22:47:27 -0800

    added a server to my index.js file

--- index.js
+++ index.js
@@ -2,7 +2,20 @@
 
 const greet = require('./lib/greet.js');
 const math = require('./lib/arithmetic.js');
+const http = require('http');
+
 
 console.log(greet.hello('JOHN'));
 console.log(math.add(1,3)); // 4
 console.log(math.subtract(1,3)); // -2
+
+const requestHandler = (req,res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.statusCode = 200;
+  res.write( 'hello world' );
+  res.end();
+};
+
+const app = http.createServer(requestHandler);
+app.listen(3000, () => console.log('server up') );
+
