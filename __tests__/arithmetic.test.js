2f064b5404e196049bf721faa80f0f279f6681eb
CaityHeath <caity.heath@gmail.com>
2018-12-17 21:58:00 -0800

    greeting test and functions written and passing

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -10,31 +10,27 @@ let b = randomNum;
 describe('adder module', () => {
   
   it('can add 2 numbers', () => {
-    let sum = arithmetic.add(a, b);
-    expect(sum).toEqual(a + b);
+    expect(arithmetic.add(a, b)).toEqual(a + b);
   });
 });
 
 describe('subtracter module', () => {
 
   it('can subtract 2 ', () => {
-    let difference = arithmetic.subtract(a, b);
-    expect(difference).toEqual(a - b);
+    expect(arithmetic.subtract(a, b)).toEqual(a - b);
   });
 });
 
 describe('multiplier module', () => {
   
   it('can multiply 2 numbers', () => {
-    let product = arithmetic.multiply(a,b);
-    expect(product).toEqual(a * b);
+    expect(arithmetic.multiply(a,b)).toEqual(a * b);
   });
 });
 
 describe('can divide 2 numbers', () => {
  
   it('can divide 2 numbers', () => {
-    let quotient = arithmetic.divide(a, b);
-    expect(quotient).toEqual(a / b);
+    expect(arithmetic.divide(a, b)).toEqual(a / b);
   });
 });
